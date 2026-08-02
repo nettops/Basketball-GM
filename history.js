ffa78b9ef5b50ad87350b2440e11d4a1062a2cef
nettops <maybachmusic3500@gmail.com>
2026-08-01 22:57:43 -0400

    feat: history.js rollSeasonIntoCareerStats (career accumulation + milestones)

--- history.js
+++ history.js
@@ -42,9 +42,58 @@ function ensureCareerData(players) {
   });
 }
 
+const MILESTONE_THRESHOLDS = {
+  points: [10000, 20000, 30000],
+  rebounds: [5000, 10000],
+  assists: [5000, 10000]
+};
+const MILESTONE_STAT_LABELS = { points: 'career points', rebounds: 'career rebounds', assists: 'career assists' };
+
+function checkMilestones(player, beforeTotals, feedSink) {
+  Object.keys(MILESTONE_THRESHOLDS).forEach(function (statKey) {
+    MILESTONE_THRESHOLDS[statKey].forEach(function (threshold) {
+      if (beforeTotals[statKey] < threshold && player.careerStats[statKey] >= threshold) {
+        feedSink(player.name + ' reaches ' + threshold.toLocaleString() + ' ' + MILESTONE_STAT_LABELS[statKey] + '.');
+      }
+    });
+  });
+}
+
+function rollSeasonIntoCareerStats(player, feedSink) {
+  const sink = feedSink || function () {};
+  ensureCareerData([player]);
+  player.peakOverall = Math.max(player.peakOverall, player.overall);
+  if (player.teamId && player.teamsPlayedFor.indexOf(player.teamId) === -1) {
+    player.teamsPlayedFor.push(player.teamId);
+  }
+
+  if (!player.seasonStats || player.seasonStats.gamesPlayed === 0) {
+    player.lastSeasonAverages = Object.assign({}, ZERO_AVERAGES);
+    return;
+  }
+
+  player.lastSeasonAverages = _HISTORY_DATA.league.getPlayerAverages(player);
+
+  ['points', 'rebounds', 'assists'].forEach(function (key) {
+    if (player.seasonStats[key] > player.bestSeasonTotals[key]) {
+      player.bestSeasonTotals[key] = player.seasonStats[key];
+    }
+  });
+
+  const beforeTotals = { points: player.careerStats.points, rebounds: player.careerStats.rebounds, assists: player.careerStats.assists };
+  player.careerStats.gamesPlayed += player.seasonStats.gamesPlayed;
+  player.careerStats.seasonsPlayed += 1;
+  _HISTORY_DATA.league.SEASON_STAT_KEYS.forEach(function (key) {
+    player.careerStats[key] += player.seasonStats[key] || 0;
+  });
+
+  checkMilestones(player, beforeTotals, sink);
+}
+
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = {
     LEAGUE_HISTORY: LEAGUE_HISTORY,
-    ensureCareerData: ensureCareerData
+    ensureCareerData: ensureCareerData,
+    rollSeasonIntoCareerStats: rollSeasonIntoCareerStats
   };
 }
